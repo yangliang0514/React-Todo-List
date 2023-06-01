196eb0dee2d80fa18d9985139a680f072080f9ad
Kevin Yang <e510021@gmail.com>
2023-06-02 00:56:57 +0800

    finish todo form logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,49 @@
-export default function App() {}
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currTodos) => {
+      return [
+        ...currTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
+
+  return (
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input type="checkbox" checked={todo.completed} />
+                {todo.title}
+              </label>
+              <button className="btn btn-danger">Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
